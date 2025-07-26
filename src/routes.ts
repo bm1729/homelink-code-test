91d9bbbc66383a027355a58180fc8ec8096a8258
bm1729 <ben.mumford1729@gmail.com>
2025-07-26 10:53:49 +0100

    extracted routes to separate plugin

---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from 'fastify';
+import { User, UserType } from './types/user';
+
+async function routes(server: FastifyInstance) {
+  //   const collection = server.mongo.db.collection('test_collection');
+
+  server.post<{ Body: UserType; Reply: UserType }>(
+    '/',
+    {
+      schema: {
+        body: User,
+        response: {
+          200: User,
+        },
+      },
+    },
+    async (request, reply) => {
+      request.log.info('Received user data:', request.body);
+      const { name, mail } = request.body;
+      reply.status(200).send({ name, mail });
+    }
+  );
+}
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import Fastify, { FastifyInstance } from 'fastify';
-import { User, UserType } from './types/user.js';
-import fastifyPlugin from 'fastify-plugin';
 import fastifyMongo from '@fastify/mongodb';
+import routes from './routes';
 
 const buildServer = (config: { mongoUri: string }): FastifyInstance => {
   const server = Fastify({
@@ -12,27 +11,8 @@ const buildServer = (config: { mongoUri: string }): FastifyInstance => {
     },
   });
 
-  async function userRoutes(server: FastifyInstance) {
-    server.post<{ Body: UserType; Reply: UserType }>(
-      '/',
-      {
-        schema: {
-          body: User,
-          response: {
-            200: User,
-          },
-        },
-      },
-      async (request, reply) => {
-        request.log.info('Received user data:', request.body);
-        const { name, mail } = request.body;
-        reply.status(200).send({ name, mail });
-      }
-    );
-  }
-
   server.register(fastifyMongo, { url: config.mongoUri });
-  server.register(userRoutes, { prefix: '/api/users' });
+  server.register(routes, { prefix: '/api/users' });
 
   return server;
 };
